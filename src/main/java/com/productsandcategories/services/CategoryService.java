1cec2060a1413115bb009e817cb9b7c18ea1354e
Brian Denmark <testingarea479@gmail.com>
2023-01-29 23:50:14 -0800

    added method to category service

--- src/main/java/com/productsandcategories/services/CategoryService.java
+++ src/main/java/com/productsandcategories/services/CategoryService.java
@@ -1,8 +1,25 @@
 package com.productsandcategories.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.productsandcategories.models.Category;
+import com.productsandcategories.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
+	@Autowired
+	CategoryRepository categoryRepo;
+	
+	public Category getOne(Long id) {
+		Optional<Category> optCategory = categoryRepo.findById(id);
+		if(optCategory.isPresent()) {
+			return optCategory.get();
+		} else {
+			return null;
+		}
+	}
 
 }
